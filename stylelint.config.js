fead7886dcded9147d6a3c29acb3f9f935f10c91
Vitaliy Vanchak <vvanchak2@gmail.com>
2018-11-03 18:20:37 +0200

    Finished basic layout + .travis.yml added

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,14 +2,19 @@ module.exports = {
   extends: [
     "stylelint-config-standard"
   ],
-  plugins: [],
-  rules: {
+  plugins: [
+    "stylelint-scss"
+  ],
+  rules:
+   {
     indentation: 2,
     "number-leading-zero": null,
     "selector-pseudo-class-no-unknown": [true, {
       ignorePseudoClasses: ["global"]
     }],
     "selector-max-id": 0,
-    "selector-class-pattern": "^([a-z][a-z0-9]*)(-[a-z0-9]+)*$"
+    "selector-class-pattern": "^([a-z][a-z0-9]*)(-[a-z0-9]+)*$",
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
   }
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -44,7 +44,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve('./index.html')
+      template: path.resolve('./src/index.html')
     }),
   ],
   devServer: {
