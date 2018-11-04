074fab72635da1c3a81cf7d3fd583194c93dc3f6
Vitaliy Vanchak <vvanchak2@gmail.com>
2018-11-04 13:15:58 +0200

    colors-list: Created build function, added description tag; Added bandsInfo file

---
+++ src/JS/bandsInfo.js
@@ -0,0 +1,103 @@
+export const bandsInfo = [
+  {
+    color: 'grey',
+    description: '8',
+    colorsList: [
+      {color: 'none', description: ''},
+      {color: 'black', description: '0'},
+      {color: 'brown', description: '1'},
+      {color: 'red', description: '2'},
+      {color: 'orange', description: '3'},
+      {color: 'yellow', description: '4'},
+      {color: 'green', description: '5'},
+      {color: 'blue', description: '6'},
+      {color: 'violet', description: '7'},
+      {color: 'grey', description: '8'},
+      {color: 'white', description: '9'}
+    ]
+  },
+  {
+    color: 'red',
+    description: '2',
+    colorsList: [
+      {color: 'none', description: ''},
+      {color: 'black', description: '0'},
+      {color: 'brown', description: '1'},
+      {color: 'red', description: '2'},
+      {color: 'orange', description: '3'},
+      {color: 'yellow', description: '4'},
+      {color: 'green', description: '5'},
+      {color: 'blue', description: '6'},
+      {color: 'violet', description: '7'},
+      {color: 'grey', description: '8'},
+      {color: 'white', description: '9'}
+    ]
+  },
+  {
+    color: 'orange',
+    description: 'x1k',
+    colorsList: [
+      {color: 'none', description: ''},
+      {color: 'black', description: '0'},
+      {color: 'brown', description: '1'},
+      {color: 'red', description: '2'},
+      {color: 'orange', description: '3'},
+      {color: 'yellow', description: '4'},
+      {color: 'green', description: '5'},
+      {color: 'blue', description: '6'},
+      {color: 'violet', description: '7'},
+      {color: 'grey', description: '8'},
+      {color: 'white', description: '9'}
+    ]
+  },
+  {
+    color: 'none',
+    description: '',
+    colorsList: [
+      {color: 'none', description: ''},
+      {color: 'black', description: 'x1'},
+      {color: 'brown', description: 'x10'},
+      {color: 'red', description: 'x100'},
+      {color: 'orange', description: 'x1K'},
+      {color: 'yellow', description: 'x10K'},
+      {color: 'green', description: 'x100K'},
+      {color: 'blue', description: 'x1M'},
+      {color: 'violet', description: 'x10M'},
+      {color: 'grey', description: 'x100M'},
+      {color: 'white', description: 'x1G'},
+      {color: 'gold', description: 'x0.1'},
+      {color: 'silver', description: 'x0.01'}
+    ]
+  },
+  {
+    color: 'gold',
+    description: '5%',
+    colorsList: [
+      {color: 'none', description: '20%'},
+      {color: 'brown', description: '1%'},
+      {color: 'red', description: '2%'},
+      {color: 'green', description: '0.5%'},
+      {color: 'blue', description: '0.25%'},
+      {color: 'violet', description: '0.1%'},
+      {color: 'grey', description: '0.05%'},
+      {color: 'gold', description: '5%'},
+      {color: 'silver', description: '10%'}
+    ]
+  },
+  {
+    color: 'none',
+    description: '',
+    colorsList: [
+      {color: 'none', description: ''},
+      {color: 'black', description: '250ppm/K'},
+      {color: 'brown', description: '100ppm/K'},
+      {color: 'red', description: '50ppm/K'},
+      {color: 'orange', description: '15ppm/K'},
+      {color: 'yellow', description: '25ppm/K'},
+      {color: 'green', description: '20ppm/K'},
+      {color: 'blue', description: '10ppm/K'},
+      {color: 'violet', description: '5ppm/K'},
+      {color: 'grey', description: '1ppm/K'}
+    ]
+  }
+];
--- src/main.js
+++ src/main.js
@@ -1,36 +1,17 @@
+import {bandsInfo} from './JS/bandsInfo';
+
 const $bandsList = document.getElementById('bands-list');
+const $colorsList = document.getElementById('colors-list');
 const $firstBand = document.getElementById('first-band');
 const $secondBand = document.getElementById('second-band');
 const $thirdBand = document.getElementById('third-band');
 const $fourthBand = document.getElementById('fourth-band');
 const $fifthBand = document.getElementById('fifth-band');
 const $sixthBand = document.getElementById('sixth-band');
+
 let selectedBandNumber = 0;
 let $bandMarker;
 let $bandDescriprion;
-const bandsObject = [
-  { color: 'grey', description: '8' },
-  { color: 'red', description: '2' },
-  { color: 'orange', description: 'x1k' },
-  { color: 'none', description: '' },
-  { color: 'gold', description: '5%' },
-  { color: 'none', description: '' }
-];
-$bandsList.onclick = event => {
-  const {target} = event;
-  const isBandBtn = target.classList.contains('bands-list__band-btn');
-  if (isBandBtn) {
-    $bandMarker = target.childNodes[1];
-    $bandDescriprion = target.childNodes[3];
-    const bandColorMarkersList = document.getElementsByClassName('bands-list__band-marker');
-    Array.prototype.forEach.call(bandColorMarkersList, element => {
-      element.className = 'bands-list__band-marker';
-    });
-    $bandMarker.className = 'bands-list__band-marker show-marker';
-    selectedBandNumber = selectBandNumberById(target.id);
-  }
-
-};
 
 export const selectBandNumberById = id => {
   const bandsList = document.getElementsByClassName('resistor__band-marker');
@@ -66,4 +47,38 @@ export const selectBandNumberById = id => {
   }
 };
 
+const createColorElement = (color, description) => {
+  const $li = document.createElement('li');
+  $li.className = 'colors-list__element';
+  $li.innerHTML = `
+    <div class="colors-list__color-circle ${color}"></div>
+    <span class="colors-list__color-name">${color}</span>
+    <span class="colors-list__color-description">${description}</span>
+  `;
+  return $li;
+};
 
+window.onload = () => {
+
+  $bandsList.onclick = event => {
+    const {target} = event;
+    const isBandBtn = target.classList.contains('bands-list__band-btn');
+    if (isBandBtn) {
+      $bandMarker = target.childNodes[1];
+      $bandDescriprion = target.childNodes[3];
+      const bandColorMarkersList = document.getElementsByClassName('bands-list__band-marker');
+      Array.prototype.forEach.call(bandColorMarkersList, element => {
+        element.className = 'bands-list__band-marker';
+      });
+      $bandMarker.className = 'bands-list__band-marker show-marker';
+      selectedBandNumber = selectBandNumberById(target.id);
+      $colorsList.innerHTML = '';
+      const bandInfoIndex = $fourthBand.classList.contains('none') && selectedBandNumber === 2 ?
+        3 : selectedBandNumber;
+      bandsInfo[bandInfoIndex].colorsList.forEach( element => {
+        const $el = createColorElement(element.color, element.description);
+        $colorsList.appendChild($el);
+      });
+    }
+  };
+};
